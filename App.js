7cf25b583c6ab3a44994e12dd6b7a5e46e70e241
mhp22 <125041399+MichaelPujos@users.noreply.github.com>
2025-02-10 21:00:52 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LaborForceChart from "./components/LaborForceChart";
+
+function App() {
+  return (
+    <div>
+      <h1>Immigration Economic Impact</h1>
+      <LaborForceChart />
+    </div>
+  );
+}
+
+export default App;
