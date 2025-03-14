b3b10a3f484f505949e47a7fb87a687b94615cdb
mhp22 <125041399+MichaelPujos@users.noreply.github.com>
2025-03-14 15:03:30 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import React from "react";
-import LaborForceChart from "./components/LaborForceChart";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Reports from "./pages/Reports";
+import "./styles/styles.css";
 
-function App() {
+
+const App = () => {
   return (
-    <div>
-      <h1>Immigration Economic Impact</h1>
-      <LaborForceChart />
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/reports" element={<Reports />} />
+      </Routes>
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
