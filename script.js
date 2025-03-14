cf9463613f101527dec990c2edb4eb2d4fe9af97
mhp22 <125041399+MichaelPujos@users.noreply.github.com>
2025-03-14 15:01:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,8 +16,14 @@ function createChart(chartId, chartType, chartLabels, chartData, chartLabel, bac
     });
 }
 
+// Original Charts
 createChart("laborForceChart", "line", [2014, 2016, 2018, 2020, 2022, 2024], [66, 67, 68, 69, 70, 71], "Labor Force Participation (%)");
-createChart("employmentChart", "bar", ["Agriculture", "Construction", "Hospitality", "Manufacturing"], [18, 22, 25, 15], "Industry Employment (%)");
-createChart("taxChart", "pie", ["Federal Taxes", "State Taxes"], [100, 60], "Tax Contributions (Billion USD)", ["blue", "red"]);
-createChart("immigrantPopulationChart", "line", [2014, 2016, 2018, 2020, 2022, 2024], [42, 45, 48, 50, 52, 54], "Total Immigrant Population (millions)");
-createChart("unauthorizedWorkforceChart", "bar", [2014, 2016, 2018, 2020, 2022, 2024], [7, 7.5, 8, 8.5, 9, 9.5], "Unauthorized Immigrant Workforce (millions)");
+createChart("employmentChart", "bar", ["Agriculture", "Construction", "Tech", "Healthcare"], [18, 22, 25, 35], "Employment by Industry (%)");
+createChart("economicContributionsChart", "pie", ["Federal Taxes", "State Taxes", "GDP Contribution"], [100, 60, 1800], "Economic Contributions (Billion USD)", ["blue", "red", "green"]);
+
+// NEW Charts
+createChart("immigrationTrendsChart", "line", [2014, 2016, 2018, 2020, 2022, 2024], [42, 45, 47, 50, 53, 55], "Total Immigrant Population (Millions)");
+createChart("workforceComparisonChart", "bar", ["Native-Born", "Foreign-Born"], [63, 72], "Workforce Participation (%)", ["orange", "blue"]);
+createChart("regionalDistributionChart", "doughnut", ["West", "South", "Midwest", "Northeast"], [40, 30, 15, 15], "Regional Distribution (%)", ["#FF6384", "#36A2EB", "#FFCE56", "#4BC0C0"]);
+createChart("educationChart", "bar", ["Less than HS", "HS Graduate", "Some College", "Bachelor's", "Graduate Degree"], [15, 25, 30, 20, 10], "Educational Attainment (%)");
+createChart("industryContributionChart", "horizontalBar", ["Healthcare", "Technology", "Manufacturing", "Construction", "Agriculture"], [120, 180, 140, 100, 80], "Industry Contribution (Billion USD)");
